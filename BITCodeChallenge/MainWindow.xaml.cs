37586f3e8eb6726d7400179003cbeb47aed56bc5
cris-russu <cris.russu@protonmail.com>
2021-01-07 11:03:02 +0100

    Add project files.

---
+++ BITCodeChallenge/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using BITCodeChallenge.Models;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows;
+using System.Xml.Serialization;
+using BITCodeChallenge.ViewModels;
+
+namespace BITCodeChallenge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string filePath;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Title = "Boyum IT Challenge";
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                openFileDialog.DefaultExt = "xml";
+                openFileDialog.Title = "Select your xml input file";
+                openFileDialog.Filter = "XML files (*.xml) |*.xml";
+                filePath = openFileDialog.FileName;
+                FilePath_TxtBox.Text = filePath;
+            }
+        }
+
+        private void btnProcessFile_Click(object sender, RoutedEventArgs e)
+        {
+            WebOrderModel webOrder = new WebOrderModel();
+            webOrder = webOrder.DeserializeToObject<WebOrderModel>(filePath);
+            WebOrder woVM = new WebOrder(webOrder);
+            viewString.Content = woVM.ToString();
+        }
+
+
+
+
+    }
+}
---
+++ BITCodeChallenge/ViewModels/WebOrder.cs
@@ -0,0 +1,44 @@
+﻿using BITCodeChallenge.Models;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BITCodeChallenge.ViewModels
+{
+    public class WebOrder
+    {
+        public int ID { get; set; }
+        public string Customer { get; set; }
+        public DateTime Date { get; set; }
+        public double Total { get; set; }
+        public double PriceAvg { get; set; }
+        public int Amount { get; set; }
+
+        public WebOrder()
+        {
+
+        }
+
+        public WebOrder(WebOrderModel webOrderModel)
+        {
+            ID = webOrderModel.ID;
+            Customer = webOrderModel.Customer;
+            Date = webOrderModel.GetParsedDate();
+            Amount = webOrderModel.Items.Sum(i => i.Quantity);
+            Total = webOrderModel.Items.Select(x => x.Price * x.Quantity).Sum();
+            PriceAvg = webOrderModel.Items.Average(i => i.Price);             
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID} {Environment.NewLine}" +
+                $"Customer: {Customer} {Environment.NewLine}" +
+                $"Date: {Date} {Environment.NewLine}" +
+                $"Price average: {PriceAvg} {Environment.NewLine}" +
+                $"Amount of items: {Amount} {Environment.NewLine}"+
+                $"Total: {Total} {Environment.NewLine}";
+        }
+
+    }
+}
